9dc0c740e9709043302da630b1497ddb923eb949
Max Dergachev <skwita.max@gmail.com>
2023-04-15 15:55:07 +0300

    Created Network Interaction #3

--- src/main/java/com/skwita/Block.java
+++ src/main/java/com/skwita/Block.java
@@ -1,35 +1,45 @@
 package com.skwita;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class Block {
+public class Block implements Serializable {
     private static int indexCounter = 0;
     private int index;
     private String prevHash;
     private String hash;
     private String data;
     private int nonce; 
+    private Timestamp timestamp;
     
     public Block() {
         this.index = indexCounter++;
         this.data = RandomStringUtils.randomAlphanumeric(256);
+        this.nonce = RandomUtils.nextInt();
     }
 
-    public boolean calculateHash() {
-        String temp = prevHash + hash + data + nonce;
-        String resultHash = DigestUtils.sha256Hex(temp);
-        if (resultHash.endsWith("0000")) {
-            this.hash = resultHash;
-            return true;
-        } else {
-            nonce += 1;
-            return false;
+    public void calculateHash() {
+        String temp;
+        String resultHash;
+        while (true) {
+            temp = prevHash + hash + data + nonce;
+            resultHash = DigestUtils.sha256Hex(temp);
+            if (resultHash.endsWith("0000")) {
+                this.hash = resultHash;
+                timestamp = new Timestamp(System.currentTimeMillis());
+                return;
+            } else {
+                nonce += 1;
+            }
         }
     }
 
@@ -39,6 +49,7 @@ public class Block {
                 "hash:" + hash + "\n" + 
                 "prevHash:" + prevHash + "\n" + 
                 "nonce:" + nonce + "\n" + 
-                "data:" + data + "\n";
+                "data:" + data + "\n" +
+                "timeStamp:" + timestamp + "\n";
     }
 }
